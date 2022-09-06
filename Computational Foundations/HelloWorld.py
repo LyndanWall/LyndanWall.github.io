e4456f4ef3df964813caaee93eff475ca8803dac
LyndanWall <89609652+LyndanWall@users.noreply.github.com>
2022-09-06 10:45:05 -0600

    activity 3

---
+++ Computational Foundations/HelloWorld.py
@@ -0,0 +1,6 @@
+var= 'hello, world!'
+print(var)
+
+a= 'hello,'
+b= ' world!'
+print(' The first computer said: ' + a + b)
---
+++ Computational Foundations/Test_Basics.py
@@ -0,0 +1,15 @@
+print(2)
+
+print(10-8)
+
+var = 80
+var/2
+var+10
+
+print(var)
+
+
+0+8
+8*8
+10/5
+
