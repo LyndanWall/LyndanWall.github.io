22de7c0e10cbe8206bf16882cf571173a6809246
LyndanWall <89609652+LyndanWall@users.noreply.github.com>
2022-10-10 16:26:36 -0600

    code review

--- Computational Foundations/PC02_Graffiti/PC02_Graffiti.py
+++ Computational Foundations/PC02_Graffiti/PC02_Graffiti.py
@@ -4,6 +4,7 @@ ATLS 1300
 Author: Dr. Z
 Author: Lyndan Wall
 DATE: 9/14/22
+REVISED DATE: 10/10/22
 DESCRIPTION: 
 Using Ada Lovelace as my background, I drew a circle, square, and line on top of her using turtle. 
 You must make sure all images you reference in the code is in the same folder as this script!
@@ -31,33 +32,43 @@ win.bgpic(bgImg) # sets the background to the selected image.
 
 #=======Add your code here======
 
-# turtle circle shape
+# drawing turtle green circle shape
 turtle.color((113,139,90))
 turtle.begin_fill()
 turtle.circle(80)
 turtle.end_fill()
 
+# compete circle and prep code to move to another shape
 turtle.penup()
 
+# moving turtle to different location
 turtle.goto(0,-160)
 turtle.pendown()
 
-#turtle square shape
+# drawing turtle blue diamolnd/square shape
 turtle.color("#4DA6FF")
 turtle.circle(80,steps=4)
 
-
+# complete square and prep code to move to another shape
 turtle.penup()
 
+# moving turtle to different location
 turtle.goto(0,160)
 turtle.pendown()
 
-#turtle line with thick line weight
+# drawing turtle line with thicker line weight
 turtle.color((77,165,255))
 turtle.pensize(4)
 
+# coding direction of arrow
 turtle.forward(80)
 
 #=======Clean up code (do not change)======
 # this code ensures that your script runs correctly each time.
 turtle.done()
+
+# Critisism sandwich:
+# positive: code works well without errors
+# negative: artistically, I could have thought through my process more in depth, 
+# My code descriptions also could have been more in depth
+# positive: I think the colors are complementary to Ada Lovelace's dress (purple)
